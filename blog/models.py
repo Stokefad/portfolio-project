897fd0a1df44674494af3f330ac8c67eaece3968
Igor-Macbook Pro <igor.naum.2013@yandex.ru>
2018-08-01 22:03:14 +0300

    Test
    The first site

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     date = models.DateTimeField(auto_now=True, auto_now_add=False)
     body = models.TextField()
     image = models.ImageField(upload_to="imagesblog/")
+
+    def summary(self):
+        return self.body[:500]
+
+    def date_pretty(self):
+        return self.date.strftime("%b %e %Y")
+
+    def __str__(self):
+        return self.title
